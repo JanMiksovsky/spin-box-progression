3b3fc81a6c9c76acb3c5c0644804415b712b23ad
Jan Miksovsky <jmiksovsky@salesforce.com>
2020-03-03 16:29:35 -0800

    Add mixin example.

---
+++ 02/mixinExample.js
@@ -0,0 +1,19 @@
+// Functional mixin
+function GreetMixin(Base) {
+  return class Greet extends Base {
+    connectedCallback() {
+      if (super.connectedCallback) {
+        super.connectedCallback();
+      }
+      console.log("Hello");
+    }
+  };
+}
+
+// Define a web component that uses the mixin.
+class GreetElement extends GreetMixin(HTMLElement) {}
+customElements.define("greet-element", GreetElement);
+
+// Instantiate the component.
+const element = new GreetElement();
+document.body.append(element); // logs "Hello"
